fc138923e5b34534369a2895898e715e733f8a9f
Mohamed_Rachdi <mohamedrachdi5634@gmail.com>
2026-08-21 14:21:03 +0100

    P: add player in the form of a triangle

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import pygame
 from logger import log_state
-from constants import SCREEN_WIDTH , SCREEN_HEIGHT
+from constants import *
+from player import Player
 def main():
     pygame.init()
     clock=pygame.time.Clock()
@@ -10,12 +11,14 @@ def main():
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    player=Player(SCREEN_WIDTH / 2,SCREEN_HEIGHT / 2)
     while True :
         log_state()
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                     return
         screen.fill("Black")
+        player.draw(screen)
         pygame.display.flip()
         dt = clock.tick(60) / 1000
         
