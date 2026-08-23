7f89c0a750f109d0505c35b9ec236d68b6682935
Mohamed_Rachdi <mohamedrachdi5634@gmail.com>
2026-08-23 02:48:13 +0100

    K : add kill

--- main.py
+++ main.py
@@ -44,6 +44,11 @@ def main():
                 log_event("player_hit")
                 print("Game over !")
                 sys.exit()
+            for shot in shots : 
+                 if shot.collides_with(asteroid) :
+                    log_event("asteroid_shot")
+                    asteroid.kill()
+                    shot.kill()
         pygame.display.flip()
         dt = clock.tick(60) / 1000
         
